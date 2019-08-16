ee5a8512f81320482d5b86946cce7d3d8c850ac0
Richard Huelves <rhuelves@gmail.com>
2019-08-16 14:38:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array(array, element) << "Vans"
+  create_an_array << "Vans"
 end
 
 def add_element_to_start_of_array(array, element)
